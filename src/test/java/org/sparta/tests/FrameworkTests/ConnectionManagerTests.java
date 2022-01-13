fdd699af0babd47f2d264776d887ebfa37935cae
mthomas1331 <mthomas6298@outlook.com>
2022-01-13 12:28:06 +0000

    Status code tests also done for ConnectionManagerTests

--- src/test/java/org/sparta/tests/FrameworkTests/ConnectionManagerTests.java
+++ src/test/java/org/sparta/tests/FrameworkTests/ConnectionManagerTests.java
@@ -1,12 +1,14 @@
 package org.sparta.tests.FrameworkTests;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.sparta.Config;
 import org.sparta.ConnectionManager;
 
 public class ConnectionManagerTests {
+
+    private final int STATUS_CODE_OK = 200;
+    private final int STATUS_CODE_BAD_REQUEST = 400;
+    private final int STATUS_CODE_NOT_FOUND = 404;
 
     @Test
     @DisplayName("Check the URL passed")
@@ -14,4 +16,39 @@ public class ConnectionManagerTests {
         ConnectionManager.buildCityNameUrl("London");
         Assertions.assertEquals("https://api.openweathermap.org/data/2.5/weather?q=London&appid="+ Config.getApiKey(), ConnectionManager.getConnection());
     }
+
+    @Nested
+    @DisplayName("Status code tests")
+    class StatusCodeTests {
+        @BeforeEach
+        void setup(){
+            ConnectionManager.buildCityNameUrl("");
+        }
+
+        @Test
+        @DisplayName("Check that the status code is 200")
+        void checkThatTheStatusCodeIs200() {
+            ConnectionManager.buildCityNameUrl("London");
+            ConnectionManager.getConnection();
+            Assertions.assertEquals(STATUS_CODE_OK, ConnectionManager.getStatusCode());
+        }
+
+        @Test
+        @DisplayName("Check that the status code is 400")
+        void checkThatTheStatusCodeIs400() {
+            ConnectionManager.getConnection();
+            Assertions.assertEquals(STATUS_CODE_BAD_REQUEST, ConnectionManager.getStatusCode());
+        }
+
+        @Test
+        @DisplayName("Check that the status code is 404")
+        void checkThatTheStatusCodeIs404() {
+            ConnectionManager.buildCityNameUrl("rty");
+            ConnectionManager.getConnection();
+            Assertions.assertEquals(STATUS_CODE_NOT_FOUND, ConnectionManager.getStatusCode());
+
+        }
+    }
+
+
 }
