3d19b768f73193d45b2f0b2207b8587176fb826d
Harry Jones <harryrjones@yahoo.com>
2022-01-13 11:44:03 +0000

    Written coordinate tests

---
+++ src/test/java/org/sparta/tests/APITests/WeatherDTOTests.java
@@ -0,0 +1,31 @@
+package org.sparta.tests.APITests;
+
+import org.junit.jupiter.api.*;
+import org.sparta.Injector;
+import org.sparta.dtos.WeatherDTO;
+
+
+public class WeatherDTOTests {
+    WeatherDTO weatherDTO = new WeatherDTO();
+
+    @BeforeEach
+    void setup(){
+        weatherDTO = Injector.injectDTO(ConnectionManager.getConnection());
+    }
+
+    @Nested
+    @DisplayName("Coordinate Tests")
+    class CoordinateTests {
+        @Test
+        @DisplayName("Longitude Range Test")
+        void longitudeRangeTest() {
+            Assertions.assertTrue(weatherDTO.getCoord().getLon() <= 180 && weatherDTO.getCoord().getLon() >= -180);
+        }
+
+        @Test
+        @DisplayName("Latitude Range Test")
+        void latitudeRangeTest() {
+            Assertions.assertTrue(weatherDTO.getCoord().getLat() <= 90 && weatherDTO.getCoord().getLat() >= -90);
+        }
+    }
+}
